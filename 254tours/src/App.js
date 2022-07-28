bd57e8a085796caa3eb9410145ccfea54b5aea41
kelvinachiando <kelvinostin22gmail.com>
2022-07-28 22:48:09 +0300

    updated the app.js file

--- 254tours/src/App.js
+++ 254tours/src/App.js
@@ -1,13 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
 const url = "http://localhost:5000/tours";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const updatedTours = tours.filter((tour) => tour.id !== id);
+    setTours(updatedTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            reload
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
-    <div>
-      <h2>Hello react</h2>
-    </div>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
